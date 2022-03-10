8f8b89a07df8eea3b08b29cc56c9e776861fa869
rudeigerc <rudeigerc@gmail.com>
2022-03-10 21:07:41 +0800

    feat: init e2e tests

---
+++ test/e2e/main_test.go
@@ -0,0 +1,53 @@
+package e2e
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	schedulingv1alpha1 "github.com/moirai-io/moirai-operator/api/v1alpha1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog/v2"
+
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
+)
+
+var (
+	testenv env.Environment
+	image   = os.Getenv("IMAGE")
+)
+
+func TestMain(m *testing.M) {
+	utilruntime.Must(schedulingv1alpha1.AddToScheme(scheme.Scheme))
+
+	testenv = env.NewWithConfig(envconf.New())
+	// Create KinD Cluster
+	kindClusterName := envconf.RandomName("moirai", 16)
+	namespace := envconf.RandomName("moirai-ns", 16)
+
+	testenv.Setup(
+		envfuncs.CreateKindClusterWithConfig(kindClusterName, "kindest/node:v1.22.5", "kind-config.yaml"),
+		envfuncs.CreateNamespace(namespace),
+		envfuncs.LoadDockerImageToCluster(kindClusterName, image),
+		deployManifest(namespace),
+	).Finish(
+		envfuncs.DeleteNamespace(namespace),
+		envfuncs.DestroyKindCluster(kindClusterName),
+	)
+	os.Exit(testenv.Run(m))
+}
+
+func deployManifest(namespace string) env.Func {
+	return func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
+		_, err := cfg.NewClient()
+		if err != nil {
+			klog.ErrorS(err, "Failed to create new Client")
+			return ctx, err
+		}
+
+		return ctx, nil
+	}
+}
